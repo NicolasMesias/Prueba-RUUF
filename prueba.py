ff4f77080fe322af8c6746e4ce58c67b4a7f01f4
NicolasMesias <115192936+NicolasMesias@users.noreply.github.com>
2024-04-23 03:34:38 -0400

    Cambios de codigo
    Segundo intento de la prueba RUUF

--- prueba.py
+++ prueba.py
@@ -1,36 +1,42 @@
 def calculo_n ():
+    print("-" * 60)
 
-    pregunta = input("Para calcular en un techo Triangular, ingrese T / Para techo Rectangular, ingrese R: ")
-    pregunta = pregunta.upper()
+    try:
+        a = int(input("Ingrese alto de panel: "))
+        b = int(input("Ingrese ancho de panel: "))
+        x = int(input("Ingrese alto de techo: "))
+        y = int(input("Ingrese ancho de techo: "))
 
-    if pregunta != "R" and pregunta != "T":
-        print("Opcion no valida")
+        if a<=0 or b<=0 or x<=0 or y<=0:
+            print("-" * 60)
+            print("Porfavor, ingrese un numero positivo")
+            print("-" * 60)
+            return
+
+    except ValueError:
+        print("-" * 60)
+        print("Porfavor, ingrese un numero valido")
+        print("-" * 60)
         return
 
-    elif pregunta == "R":
-    
-        a = int(input("Ingrese alto de panel: "))
-        b = int(input("Ingrese ancho de panel: "))
+    print("-" * 60)
 
-        x = int(input("Ingrese alto de techo: "))
-        y = int(input("Ingrese ancho de techo: "))
+    if a>x and b>x:
+        print("Las dimensiones del techo no cumplen con el alto de un panel")
+        print("-" * 60)
+        return
+    elif a>y and b>y:
+        print("Las dimensiones del techo no cumplen con el ancho de un panel")
+        print("-" * 60)
+        return
 
-        dim_paneles = a*b
-        dim_techo = x*y
+    dim_paneles = a*b
+    dim_techo   = x*y
 
-        cantidad = dim_techo//dim_paneles
+    cantidad = dim_techo//dim_paneles
 
-    elif pregunta == "T":
-        a = int(input("Ingrese alto de panel: "))
-        b = int(input("Ingrese ancho de panel: "))
+    mensaje = f"En un techo de dimensiones {x}x{y} tienen capacidad para: {cantidad} paneles \n{'-' * 60}"
 
-        x = int(input("Ingrese alto de techo: "))
-        y = int(input("Ingrese ancho de techo: "))
-
-        dim_paneles = a*b
-        dim_techo   = x*y*0.5
-        cantidad    = dim_techo//dim_paneles
-
-    return print(cantidad)
+    print(mensaje)
 
 calculo_n()
